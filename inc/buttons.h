d32eb1f99ca7809bae097d4198e05b5e66c58d58
Tunnerk <kristof.tunner@propatria-inc.com>
2021-11-07 12:59:33 +0100

    bare-metal app working(without touchGFX)

---
+++ inc/buttons.h
@@ -0,0 +1,12 @@
+/*
+ * buttons.h
+ *
+ *  Created on: Nov 1, 2021
+ *      Author: krist
+ */
+
+#ifndef TESLACTRLINC_BUTTONS_H_
+#define TESLACTRLINC_BUTTONS_H_
+
+
+#endif /* TESLACTRLINC_BUTTONS_H_ */
